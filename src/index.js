20165d6515402047255aa593ab40052d3b03e104
Uros Ivanovic <urosivanovic@Uross-MBP.lan>
2021-11-30 13:50:03 -0500

    early start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './less/index.less'
 
-// Your code goes here!
+// Your code goes here! !!!
+
+const footer = document.querySelector('footer');
+
+footer.addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor = 'violet'
+});
